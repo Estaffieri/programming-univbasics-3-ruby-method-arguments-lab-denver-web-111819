eb5661aff2a531af8f1591d7a9f74ea9c443345d
Estelle S <estellelareinne@gmail.com>
2019-11-12 21:11:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,32 +3,11 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-def introduction (Devin)
-  puts "Hi, my name is #{introduction}"
-end  
-
-describe "#introduction" do
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
-    expect{introduction("Devin")}.to output("Hi, my name is Devin.\n").to_stdout
-    expect{introduction("Elosie")}.to output("Hi, my name is Elosie.\n").to_stdout
-  end
-end  
-
-describe "#introduction" do
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
-    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-    expect{introduction("Dan")}.to output("Hi, my name is Dan.\n").to_stdout
-  end
+ def introduction(name)
+   puts "Hi, my name is #{name}."
 end
 
-describe "#introduction_with_language" do
-  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
-    expect{introduction("Devin , Ruby")}. to output (" Hi, my name is Devin and I am learning to program in Ruby.\n").to_stdout
-    expect{introduction("Elosie , Ruby")}. to output (" Hi, my name is Elosie and I am learning to program in Ruby.\n").to_stdout
-  end
-end  
-
-
+introduction("Estelle")
 
 
 
