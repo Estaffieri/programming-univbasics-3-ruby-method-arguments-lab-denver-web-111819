b1d884644871ae52c0282b8c034353c33906eda4
Estelle S <estellelareinne@gmail.com>
2019-11-12 21:14:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,12 @@ end
 
 introduction("Estelle")
 
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+introduction_with_language("Estelle", "Ruby")
+
 
 
 
